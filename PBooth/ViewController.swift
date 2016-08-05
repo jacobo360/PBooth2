5f38ec90f5ce7aa10aa167a3bb070f54544a6e84
Jacobo Koenig <jacobo360@gmail.com>
2016-08-05 10:48:43 -0400

    Initial Commit

---
+++ PBooth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PBooth
+//
+//  Created by Jacobo Koenig on 8/5/16.
+//  Copyright © 2016 Jacobo Koenig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
