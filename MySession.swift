b52b07a8d4963db09fd9b0539d5d32b13dae2bce
Jacobo Koenig <jacobo360@gmail.com>
2016-08-14 21:51:56 -0400

    Before changing the way things work

--- MySession.swift
+++ MySession.swift
@@ -26,8 +26,7 @@ class MySession: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
         getImages()
         tableView.registerForDraggedTypes([NSGeneralPboard])
         
-        let gif = GIFMaker().createGIF(with: images, frameDelay: 0.2)
-        //GIFMaker().saveGIF(gif)
+        //GIFMaker().createGIF(with: images, frameDelay: 0.2)
     }
     
     func getImages() {
